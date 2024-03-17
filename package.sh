1dd243b8172c87c01bc045a6ea0ba81b9b2c214d
kujirahand <web@kujirahand.com>
2024-03-17 19:58:40 +0900

    v0.1.2

---
+++ package.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "[Package]"
+echo "https://packaging.python.org/en/latest/tutorials/packaging-projects/"
+
+# clean
+rm -f -r dist
+rm -f -r tkeasygui.egg-info
+
+# build
+echo "--- build ---"
+python3 -m build
+
+# test install
+echo "--- upload test repo ---"
+python3 -m twine upload --repository testpypi dist/*
+echo "--- install test repo ---"
+python3 -m pip install -U --index-url https://test.pypi.org/simple/ --no-deps tkeasygui
+
+# echo "--- upload pypi ---"
+# python3 -m twine upload dist/*
+
