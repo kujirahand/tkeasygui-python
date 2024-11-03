24d7370034050de886be4369c3be8cb12139d8cc
kujirahand <web@kujirahand.com>
2024-11-04 01:50:03 +0900

    add tests/image_size_test.py

---
+++ tests/image_size_test.py
@@ -0,0 +1,29 @@
+import TkEasyGUI as eg
+
+
+def load_image(window, filename):
+    im: eg.Image = window["-image-"]
+    im.update(filename=filename, resize_type=eg.ImageResizeType.FIT_BOTH, background_color=(255, 255, 255))
+
+
+def main():
+    # create window
+    window = eg.Window("Hello World", layout=[
+        [eg.Text("Please select an image file")],
+        [eg.Input(key="-file-", enable_events=True), eg.FileBrowse()],
+        [eg.Image(key="-image-")],
+        [eg.Button("Close")]
+    ])
+    load_image(window, "a.png")
+    # event loop
+    while True:
+        event, values = window.read()
+        print(event, values)
+        if event == "-file-":
+            filename = values["-file-"]
+            load_image(window, filename)
+        if event in [eg.WIN_CLOSED, "Close"]:
+            break
+
+if __name__ == "__main__":
+    main()
