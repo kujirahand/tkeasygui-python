12c7ca46f363ec94b1efbef53c6ef36bcc733b99
kujirahand <web@kujirahand.com>
2025-04-13 23:34:27 +0900

    fixed tests/element_justification_test.py

--- tests/element_justification_test.py
+++ tests/element_justification_test.py
@@ -1,21 +1,20 @@
-"""
-element_justification test
-"""
+"""element_justification test"""
 
 import TkEasyGUI as eg
 
 win = eg.Window(
-    title="Push Test",
+    title="Center Test",
     layout=[
-        [eg.Text("=" * 50)],
-        [eg.Button("Right")],
+        [eg.Text("-" * 50)],
+        [eg.Text("This is a test of the element justification.")],
+        [eg.Text("-" * 50)],
         [eg.Button("Center")],
+        [eg.Button("Close")],
     ],
-    element_justification="center", # center alignment
+    element_justification="center",  # center alignment
 )
 while win.is_running():
     event, values = win.read()
-    if event == "Center" or event == "Right":
+    if event == "Center" or event == "Close":
         break
 win.close()
-
