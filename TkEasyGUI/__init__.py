27481d548b495f62b879cde03548fbe30b2d429b
kujirahand <web@kujirahand.com>
2025-10-07 23:50:12 +0900

    eg.*のメソッドが減っている問題を修正

--- TkEasyGUI/__init__.py
+++ TkEasyGUI/__init__.py
@@ -6,6 +6,8 @@ from .locale_easy import *  # noqa: F403, I001
 from .utils import *  # noqa: F403, I001
 from .version import __version__  # noqa: F401, I001
 from .widgets import *  # noqa: F403, I001
+from .widgets_image import *  # noqa: F403, I001
+from .widgets_window import *  # noqa: F403, I001
 
 if __name__ == "__main__":
     print(__doc__)
