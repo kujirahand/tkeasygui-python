f837dd150c757dddcf70f2428f4a27744624fff1
kujirahand <web@kujirahand.com>
2025-10-07 17:34:46 +0900

    fixed lint problem for #139

--- docs/scripts/icon/make_icon.py
+++ docs/scripts/icon/make_icon.py
@@ -1,6 +1,9 @@
-import os
-from PIL import Image
+"""Generate icon files and embed icon as base64 in a Python file."""
+
 import base64
+import os
+
+from PIL import Image
 
 icon_dir = os.path.dirname(__file__)
 script_dir = os.path.dirname(icon_dir)
@@ -18,14 +21,18 @@ py_path = os.path.join(root_dir, "TkEasyGUI", "icon.py")
 # Open the source image
 img = Image.open(src_path)
 # Save as ICO with the requested sizes
-img.save(dest_path, format="ICO", sizes=[(16, 16), (32, 32), (48, 48), (64, 64), (128, 128), (256, 256)])
+img.save(
+    dest_path,
+    format="ICO",
+    sizes=[(16, 16), (32, 32), (48, 48), (64, 64), (128, 128), (256, 256)],
+)
 
 # Resize to 256x256
-img256 = img.resize((256, 256), Image.LANCZOS)
+img256 = img.resize((256, 256), Image.Resampling.LANCZOS)
 img256.save(png256_path, format="PNG")
 
 # Resize to 64x64
-img = img.resize((64, 64), Image.LANCZOS)
+img = img.resize((64, 64), Image.Resampling.LANCZOS)
 img.save(png_path, format="PNG")
 
 # Read the ICO file and encode it to base64
@@ -34,10 +41,11 @@ with open(png_path, "rb") as ico_path:
     # The result is a string that can be used in a data URI
     # Example: "data:image/x-icon;base64,BASE64_ENCODED_STRING"
     b64_string = base64.b64encode(ico_path.read()).decode("ascii")
-    src = f"""
+
+ICON_BASE64_ENCODED = f"""
 \"\"\"TkEasyGUI icon\"\"\"
 ICON = b"{b64_string}"
 """
-    with open(py_path, "w", encoding="utf-8") as fp:
-        fp.write(src)
-    print(src)
+with open(py_path, "w", encoding="utf-8") as fp:
+    fp.write(ICON_BASE64_ENCODED)
+    print(ICON_BASE64_ENCODED)
