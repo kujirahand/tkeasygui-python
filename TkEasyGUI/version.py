04ca2f95d37a35cb75a5d814a5394412c8aaab65
kujirahand <web@kujirahand.com>
2025-10-08 00:28:52 +0900

    v1.0.40

--- TkEasyGUI/version.py
+++ TkEasyGUI/version.py
@@ -1,7 +1,7 @@
 # pylint: disable=line-too-long
 """
-# TkEasyGUI version 1.0.39
+# TkEasyGUI version 1.0.40
 
 audo generated from [pyproject.toml](https://github.com/kujirahand/tkeasygui-python/blob/main/pyproject.toml) by update_version.py
 """
-__version__ = "1.0.39"
+__version__ = "1.0.40"
--- docs/TkEasyGUI/version-py.md
+++ docs/TkEasyGUI/version-py.md
@@ -1,6 +1,6 @@
 # Module TkEasyGUI.version
 
-# TkEasyGUI version 1.0.39
+# TkEasyGUI version 1.0.40
 
 audo generated from [pyproject.toml](https://github.com/kujirahand/tkeasygui-python/blob/main/pyproject.toml) by update_version.py
 
