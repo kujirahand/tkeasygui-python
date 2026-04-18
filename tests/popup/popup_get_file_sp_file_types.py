674a92dd443c13d2309f90266c2709b6906f623d
kujirahand <web@kujirahand.com>
2026-04-18 14:36:27 +0900

    add test for #148

--- tests/popup/popup_get_file_sp_file_types.py
+++ tests/popup/popup_get_file_sp_file_types.py
@@ -4,6 +4,8 @@
 
 import TkEasyGUI as eg
 
+FILES_DELIMITER = "|"
+
 # file types
 file_types = (
     ("Image files", "*.jpg;*.jpeg;*.jpe;*.heic;*.png;*.gif"),
@@ -14,5 +16,8 @@ files = eg.popup_get_file(
     "Please select images.",
     file_types=file_types,
     multiple_files=True,
+    files_delimiter=FILES_DELIMITER,
 )
-print(files)
+print(f"raw={files}")
+if files not in (None, ""):
+    print(files.split(FILES_DELIMITER))
