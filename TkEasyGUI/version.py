4e3ea4534d153b9e6c50c828a0eafcca0ff724f1
kujirahand <web@kujirahand.com>
2026-07-17 09:02:46 +0900

    Makefile to just #168

--- TkEasyGUI/version.py
+++ TkEasyGUI/version.py
@@ -2,6 +2,6 @@
 """
 # TkEasyGUI version 1.0.44
 
-audo generated from [pyproject.toml](https://github.com/kujirahand/tkeasygui-python/blob/main/pyproject.toml) by update_version.py
+Auto generated from [pyproject.toml](https://github.com/kujirahand/tkeasygui-python/blob/main/pyproject.toml) by update_version.py
 """
 __version__ = "1.0.44"
